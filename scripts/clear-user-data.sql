0566882a40842178abf9cf8955d7f30839e529e5
AlexRasmussen <ar@cajo-dk>
2026-01-06 22:57:38 +0100

    Updated webmanifest

---
+++ scripts/clear-user-data.sql
@@ -0,0 +1,18 @@
+TRUNCATE TABLE
+  collection_import_rows,
+  collection_imports,
+  collection_items,
+  market_data,
+  matrices,
+  packaging,
+  pressings,
+  tracks,
+  album_genres,
+  album_styles,
+  albums,
+  artists,
+  media_assets,
+  external_references,
+  discogs_cache_pages,
+  discogs_cache_releases
+CASCADE;
